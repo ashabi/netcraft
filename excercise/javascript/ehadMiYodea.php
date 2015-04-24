e5b71e1c736cfdb43b928b270c6cb9f71d75ca0c
ashabi <adina.shabi@gmail.com>
2015-04-24 09:20:12 +0300

    first commit

---
+++ excercise/javascript/ehadMiYodea.php
@@ -0,0 +1,36 @@
+<?php
+
+
+function ehadMiYodea () {
+	var times = 0;
+	var answers = [
+		"elohenu",
+		"luhot ha brit",
+		"avot",
+		"imahot",
+		"humshei tora",
+		"sidrei mishna",
+		"yamei shabta",
+		"yamei mila",
+		"yerhei leida",
+		"divraya",
+		"kochvaya",
+		"shivtaya",
+		"midaya"
+	];
+
+
+	function question (n) {
+
+		foreach ($question as $key=>$value){
+			print $value;
+		}
+	}
+
+}
+	
+
+
+ehadMiYodea();
+
+?>
---
+++ excercise/javascript/js/main.js
@@ -0,0 +1,7 @@
+var age = 10;
+function sayHello(name, age){
+	console.log('Hello' + name);
+	console.log('age', age);
+}
+sayHello('Daisy');
+console.log(age);
